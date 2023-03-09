8b707747d5b4da3778aa2b1c2a1a69bfb4cc6b9d
SunitaDutta <49290085+SunitaDutta@users.noreply.github.com>
2023-03-09 17:08:17 +0530

    implement config server

---
+++ centralized-config/limit-service/src/main/java/com/spring/microservice/limitservice/controller/LimitController.java
@@ -0,0 +1,23 @@
+package com.spring.microservice.limitservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.microservice.limitservice.bean.Limits;
+import com.spring.microservice.limitservice.config.Configuration;
+
+@RestController
+public class LimitController {
+	
+	@Autowired
+	private Configuration config;
+	
+	@GetMapping("/limits")
+	public Limits retrieveLimits() {
+		
+		//return new Limits(1,1000);
+		return new Limits(config.getMinimum(),config.getMaximum());
+	}
+
+}
